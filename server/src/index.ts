d9351312f4f18c965a314b4b8f5899b71401f5a5
chenxiaoxu <v_chenxiaoxu01@baidu.com>
2021-02-08 20:10:02 +0800

    build

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,9 @@
 import "reflect-metadata";
 import Express from "express";
-import { Movie } from "./entities/Movie";
-import { SearchCondition } from "./entities/SearchCondition";
-import { MovieService } from "./services/MovieService";
 import MovieRouter from "./routes/MovieRoute";
 import UploadRouter from "./routes/UploadRoute";
 import path from 'path';
+import history from "connect-history-api-fallback";
 
 const condition: any = { page: 1, limit: 5, key: '电影名称1' }
 
@@ -13,9 +11,12 @@ const condition: any = { page: 1, limit: 5, key: '电影名称1' }
 const app = Express();
 
 app.use(Express.json()); // 配置中间件，用于解析消息体中的json格式数据
+app.use(history());
+
+
+app.use("/", Express.static(path.resolve(__dirname, '../public/build/')));
 
 app.use("/api/movie", MovieRouter);
 app.use("/api/upload", UploadRouter);
-app.use("/api/test/", Express.static(path.resolve(__dirname, '../public/uploads/')));
 
 app.listen(8998, () => console.log('server is listening on 8998'));
